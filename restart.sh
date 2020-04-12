e0db8110f81218cc949408415c98929ff1dea6e7
ivan tkachenko <me@ratijas.tk>
2020-04-12 05:08:10 +0300

    change restart script: docker-compose build -> pull

--- restart.sh
+++ restart.sh
@@ -2,7 +2,13 @@
 
 set -e
 echo ':: restarting...'
-docker-compose build
+
+# @multi_vote_bot is now hosting on docker hub.
+# But if you are developing your own fork, you would prefer to build from sources,
+# or at least pull from another repository.
+docker-compose pull
+#docker-compose build
+
 docker-compose down --remove-orphans
 ./backup.sh
 docker-compose up -d
